b16a72a2cf64dc915f46bf0fbf75bb47e366dadc
glenntosco <glenn@barrdega.com>
2024-12-07 16:54:34 -0500

    computer change

--- Server/obj/Debug/net9.0/ActivosFiljos.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/ActivosFiljos.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaa27632ea37ec8cbd1c5b7f5f8f9e167fa4a705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f39aa42a01d8733189132b87e0f428a621f5a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
