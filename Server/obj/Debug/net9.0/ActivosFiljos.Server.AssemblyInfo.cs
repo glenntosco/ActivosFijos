18a58a93a89667ae47f71d7453cd025949861f75
glenntosco <glenn@barrdega.com>
2024-12-06 19:37:46 -0500

    dark

--- Server/obj/Debug/net9.0/ActivosFiljos.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/ActivosFiljos.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77f1497103a69e11d62e2615d2f1263da6c8e29b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6f47c2981d6117ab174a404f0d96b6464976d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
