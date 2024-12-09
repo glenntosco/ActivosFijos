8a8f594b29145b1579fe3677482f3195b8b79904
glenntosco <glenn@barrdega.com>
2024-12-09 08:53:44 -0500

    expanded grid

--- Server/obj/Debug/net9.0/ActivosFiljos.Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/ActivosFiljos.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e21e6299b69ea52842749f161c17366261174e14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+607c583cb00bf529f5ba4234b28cf292cb2b88ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActivosFiljos.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
